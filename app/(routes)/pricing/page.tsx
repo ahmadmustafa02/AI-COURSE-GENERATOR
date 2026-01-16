eb50834205be83cfcc1d121506232da598db04cf
Ahmad Mustafa <ahmadmustafabutt01@gmail.com>
2026-01-16 09:37:13 -0800

    Payment Gateway Integrated

---
+++ app/(routes)/pricing/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {PricingTable} from '@clerk/nextjs'
+function Pricing () {
+  return (
+    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '0 1rem' }}>
+    <PricingTable />
+  </div>
+  )
+}
+export default Pricing
